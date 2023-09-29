4921e1361b00b4718c1a52ce06f349e16746f335
Cristian Diaz <ismael-cris@hotmail.com>
2023-09-29 17:45:05 -0300

    git commit

---
+++ proyectoIntegrador.js
@@ -0,0 +1,9 @@
+document.getElementById("boton1").addEventListener("click", function() {
+    console.log("Welcome to my CV!!");
+    document.getElementById("demo").innerHTML = "Welcome to my CV!!";
+});
+
+document.getElementById("boton2").addEventListener("click", function() {
+    console.log("O haz click aqui!!");
+    document.getElementById("demo").innerHTML = "Bienvenidos a mi CV!!";
+});
